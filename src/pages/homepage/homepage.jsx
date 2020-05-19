c6985b33d847b3bc11efe4e85c49fb0248d334df
Anil2389 <anildeepu13@gmail.com>
2020-05-19 01:30:25 -0400

    new clothing

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './homepage.styles.scss'
+import Directory from '../../component/directory-menu/directory-menu';
+
+const Homepage = () => (
+    <div className="homepage">
+        <Directory />
+    </div>
+)
+
+export default Homepage;
